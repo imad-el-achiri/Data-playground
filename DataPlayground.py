dfa85f9e7ef3a5b9191a49062e050fee37040caa
imad-el-achiri <58442202+imad-el-achiri@users.noreply.github.com>
2025-01-06 01:03:59 +0100

    Add files via upload

---
+++ DataPlayground.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pandas as pd
+
+from ollama import chat
+from ollama import ChatResponse
+from tools import dfPrinter, scatterPlot, summarize, histogram
+
+st.title("Data playground")
+
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+
+prompt = st.text_area(label = "", placeholder = "Enter your prompt here")
+
+messages = [{'role': 'user', 'content': prompt}]
+
+available_functions = {
+  "dfPrinter": dfPrinter,
+  "scatterPlot": scatterPlot,
+  "summarize": summarize,
+  "histogram":histogram
+}
+
+response: ChatResponse = chat(
+  'llama3.2:1b',
+  messages=messages,
+  tools=[dfPrinter, scatterPlot, summarize, histogram],
+)
+
+if response.message.tool_calls:
+  # There may be multiple tool calls in the response
+  for tool in response.message.tool_calls:
+    # Ensure the function is available, and then call it
+    if function_to_call := available_functions.get(tool.function.name):
+      print('Calling function:', tool.function.name)
+      print('Arguments:', tool.function.arguments)
+      function_to_call(**tool.function.arguments)
---
+++ tools.py
@@ -0,0 +1,55 @@
+import inspect
+import pandas as pd
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def dfPrinter(*args, **kwargs) -> None:
+    """
+    prints a dataframe
+    Args:
+      This function does not take any parameters, call it with none
+    """
+
+    calling_frame = inspect.stack()[1].frame
+    calling_globals = calling_frame.f_globals
+    df = calling_globals["df"]
+    st.write(df)
+
+def scatterPlot(column1: str, column2: str) -> None:
+    """
+    prints a scatterplot of two dataframe columns
+    Args:
+      column1 (str): The first column
+      column2 (str): the second column
+    """
+    calling_frame = inspect.stack()[1].frame
+    calling_globals = calling_frame.f_globals
+    df = calling_globals["df"]
+    st.scatter_chart(data=df, x=column1, y=column2, x_label=column1, y_label=column2)
+
+def summarize(column: str) -> None:
+    """
+    Calculates a summary of statistics about the column from the dataframe
+    Args:
+      column (str): The column to do a statistical summary about
+    """
+    calling_frame = inspect.stack()[1].frame
+    calling_globals = calling_frame.f_globals
+    df = calling_globals["df"]
+    st.write(df[[column]].describe())
+
+def histogram(column: str) -> None:
+    """
+    draws/plots a histogram/barplot/ditribution plot about the column from the dataframe
+    Args:
+      column (str): The column to do plot the histogram for
+    """
+    calling_frame = inspect.stack()[1].frame
+    calling_globals = calling_frame.f_globals
+    df = calling_globals["df"]
+    fig, ax = plt.subplots()
+    ax.hist(df[column], bins=20)
+
+    st.pyplot(fig)
